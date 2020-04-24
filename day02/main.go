85532b5f468fe7a1c79c02316ebbca3ded5303f9
Felix Hillingshaeuser <felix@mxcore.de>
2020-04-24 17:46:07 +0200

    add day02 part 2

--- day02/main.go
+++ day02/main.go
@@ -18,6 +18,28 @@ func intArrayMin(input []int) int {
 	return min
 }
 
+func intArrayTwoMin(input []int) (int, int) {
+	minKey := 0
+	min1 := input[0]
+	for k, v := range input {
+		if v < min1 {
+			minKey = k
+			min1 = v
+		}
+	}
+
+	input = append(input[:minKey], input[minKey+1:]...)
+
+	min2 := input[0]
+	for _, v := range input {
+		if v < min2 {
+			min2 = v
+		}
+	}
+
+	return min1, min2
+}
+
 func calculateWrappingPaper(length int, width int, height int) int {
 	sides := make([]int, 3)
 	sides[0] = length * width
@@ -39,12 +61,30 @@ func extractSizes(input string) (int, int, int) {
 	return length, width, height
 }
 
+func calculateRibbon(length int, width int, height int) int {
+	sides := make([]int, 3)
+	sides[0] = length
+	sides[1] = width
+	sides[2] = height
+
+	min1, min2 := intArrayTwoMin(sides)
+	wrap := min1*2 + min2*2
+
+	bow := length * width * height
+
+	return wrap + bow
+}
+
 func main() {
 	file, _ := os.Open("input.txt")
 	scanner := bufio.NewScanner(file)
-	sum := 0
+	sumWrappingPaper := 0
+	sumRibbon := 0
 	for scanner.Scan() {
-		sum += calculateWrappingPaper(extractSizes(scanner.Text()))
+		l, w, h := extractSizes(scanner.Text())
+		sumWrappingPaper += calculateWrappingPaper(l, w, h)
+		sumRibbon += calculateRibbon(l, w, h)
 	}
-	fmt.Println(sum)
+	fmt.Println(sumWrappingPaper)
+	fmt.Println(sumRibbon)
 }
