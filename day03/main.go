db2df5bea167ce4532a170321077b4a467d7cb23
Felix Hillingshaeuser <felix@mxcore.de>
2020-04-25 13:23:13 +0200

    add day03 part 2

--- day03/main.go
+++ day03/main.go
@@ -12,6 +12,7 @@ type House struct {
 }
 
 func main() {
+	// part1
 	var moves string
 	houses := make(map[House]int, 1000)
 	x := 0
@@ -22,6 +23,7 @@ func main() {
 	for scanner.Scan() {
 		moves = scanner.Text()
 	}
+	file.Close()
 
 	houses[House{x, y}]++
 
@@ -44,6 +46,60 @@ func main() {
 
 		if len(moves) > 0 {
 			moves = moves[1:len(moves)]
+		} else {
+			moves = ""
+		}
+	}
+
+	fmt.Println(len(houses))
+
+	// part2
+	houses = make(map[House]int, 1000)
+	xS := 0
+	yS := 0
+	xR := 0
+	yR := 0
+
+	file, _ = os.Open("input.txt")
+	scanner = bufio.NewScanner(file)
+	for scanner.Scan() {
+		moves = scanner.Text()
+	}
+	file.Close()
+
+	houses[House{xS, yS}]++
+	houses[House{xR, yR}]++
+
+	for {
+		if len(moves) == 0 {
+			break
+		}
+
+		if moves[0:1] == ">" {
+			xS++
+		} else if moves[0:1] == "<" {
+			xS--
+		} else if moves[0:1] == "^" {
+			yS++
+		} else if moves[0:1] == "v" {
+			yS--
+		}
+
+		if moves[1:2] == ">" {
+			xR++
+		} else if moves[1:2] == "<" {
+			xR--
+		} else if moves[1:2] == "^" {
+			yR++
+		} else if moves[1:2] == "v" {
+			yR--
+		}
+
+		houses[House{xS, yS}]++
+		houses[House{xR, yR}]++
+
+		if len(moves) > 0 {
+			moves = moves[2:len(moves)]
 		} else {
 			moves = ""
 		}
