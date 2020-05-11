acb6755d2c39750bec793e01bbdb3fe727f21bc8
Felix Hillingshaeuser <felix@mxcore.de>
2020-05-11 08:46:26 +0200

    add day12 part 2

--- day12/main.go
+++ day12/main.go
@@ -24,6 +24,33 @@ func getIntSum(value interface{}) int {
 	return sum
 }
 
+func getIntSumWithoutRed(value interface{}) (int, bool) {
+	sum := 0
+	switch value.(type) {
+	case string:
+		if value.(string) == "red" {
+			return 0, false
+		}
+	case float64:
+		sum += int(value.(float64))
+	case []interface{}:
+		for _, v := range value.([]interface{}) {
+			if childSum, ok := getIntSumWithoutRed(v); ok {
+				sum += childSum
+			}
+		}
+	case map[string]interface{}:
+		for _, v := range value.(map[string]interface{}) {
+			if childSum, ok := getIntSumWithoutRed(v); ok {
+				sum += childSum
+			} else {
+				return 0, true
+			}
+		}
+	}
+	return sum, true
+}
+
 func main() {
 	file, _ := os.Open("input.txt")
 	scanner := bufio.NewScanner(file)
@@ -31,10 +58,11 @@ func main() {
 	var input string
 	for scanner.Scan() {
 		input = scanner.Text()
+
+		var jsonInterface interface{}
+		json.Unmarshal([]byte(input), &jsonInterface)
+
+		fmt.Println(getIntSum(jsonInterface))
+		fmt.Println(getIntSumWithoutRed(jsonInterface))
 	}
-
-	var jsonInterface interface{}
-	json.Unmarshal([]byte(input), &jsonInterface)
-
-	fmt.Println(getIntSum(jsonInterface))
 }
