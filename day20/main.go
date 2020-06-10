221e62b31b9539515e6365ae5b5e0ec85fb5ce00
Felix Hillingshaeuser <felix@mxcore.de>
2020-06-10 20:15:45 +0200

    add day20 part 2

--- day20/main.go
+++ day20/main.go
@@ -5,14 +5,27 @@ import "fmt"
 func main() {
 	number := 34000000
 	tnumber := number / 10
-	houses := make([]int, tnumber)
+	housesP1 := make([]int, tnumber)
+	housesP2 := make([]int, tnumber)
 	for i := 1; i < tnumber; i++ {
+		c := 0
 		for j := i; j < tnumber; j += i {
-			houses[j] += i * 10
+			housesP1[j] += i * 10
+			c++
+			if c < 50 {
+				housesP2[j] += i * 11
+			}
 		}
 	}
 
-	for k, v := range houses {
+	for k, v := range housesP1 {
+		if v > number {
+			fmt.Println(k)
+			break
+		}
+	}
+
+	for k, v := range housesP2 {
 		if v > number {
 			fmt.Println(k)
 			break
